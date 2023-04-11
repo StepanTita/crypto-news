333c17d7d9da34f802caa7c8be7512e6ac96fecb
stepantytarenko <stepan.tytarenko@nure.ua>
2023-04-11 16:57:11 -0400

    fix repositories naming

--- scripts/push-to-dockerhub.sh
+++ scripts/push-to-dockerhub.sh
@@ -8,11 +8,11 @@ services=("migrator" "parser" "telegram-bot" "twitter-bot")
 for IMAGE_NAME in "${services[@]}"; do
   echo "Processing $IMAGE_NAME..."
 
-  docker build -t "$IMAGE_REPO"/"$IMAGE_NAME":"$IMAGE_TAG" . -f ./"$IMAGE_NAME"/Dockerfile
-  docker push "$IMAGE_REPO"/"$IMAGE_NAME":"$IMAGE_TAG"
-  docker tag "$IMAGE_REPO"/"$IMAGE_NAME":"$IMAGE_TAG" "$IMAGE_REPO"/"$IMAGE_NAME":latest
-  docker push "$IMAGE_REPO"/"$IMAGE_NAME":latest
+  docker build -t "$IMAGE_REPO"/crypto-news-"$IMAGE_NAME":"$IMAGE_TAG" . -f ./"$IMAGE_NAME"/Dockerfile
+  docker push "$IMAGE_REPO"/crypto-news-"$IMAGE_NAME":"$IMAGE_TAG"
+  docker tag "$IMAGE_REPO"/crypto-news-"$IMAGE_NAME":"$IMAGE_TAG" "$IMAGE_REPO"/crypto-news-"$IMAGE_NAME":latest
+  docker push "$IMAGE_REPO"/crypto-news-"$IMAGE_NAME":latest
 
-  echo "Pushed" "$IMAGE_REPO"/"$IMAGE_NAME":"$IMAGE_TAG"
-  echo "Pushed" "$IMAGE_REPO"/"$IMAGE_NAME":latest
+  echo "Pushed" "$IMAGE_REPO"/crypto-news-"$IMAGE_NAME":"$IMAGE_TAG"
+  echo "Pushed" "$IMAGE_REPO"/crypto-news-"$IMAGE_NAME":latest
 done
