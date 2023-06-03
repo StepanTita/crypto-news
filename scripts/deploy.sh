059bfa96824714c4fe5252707a1839e8ad7eb464
stepantytarenko <stepan.tytarenko@nure.ua>
2023-06-03 17:25:16 -0400

    set project on deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,8 @@ ssh -o StrictHostKeyChecking=no "${REMOTE_USER}"@"${REMOTE_HOST}" -i private_key
         git reset --hard origin/${BRANCH}
         git pull origin ${BRANCH} &&
         cd ./docker &&
-        docker-compose -f docker-compose.${ENVIRONMENT}.yaml down -v --rmi all --volumes
-        docker-compose -f docker-compose.${ENVIRONMENT}.yaml pull
-        docker-compose -f docker-compose.${ENVIRONMENT}.yaml up -d
+        docker-compose -p ${ENVIRONMENT} -f docker-compose.${ENVIRONMENT}.yaml down --volumes
+        docker-compose -p ${ENVIRONMENT} -f docker-compose.${ENVIRONMENT}.yaml pull
+        docker-compose -p ${ENVIRONMENT} -f docker-compose.${ENVIRONMENT}.yaml up -d
         "
 echo 'Deployment completed successfully'
